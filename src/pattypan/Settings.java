09fe8c0f9bd0faec07d3cb2232e58724d0bcb2a3
Paweł Marynowski <yarl@o2.pl>
2016-04-14 21:48:21 +0200

    0.1

--- src/pattypan/Settings.java
+++ src/pattypan/Settings.java
@@ -31,7 +31,7 @@ public final class Settings {
   private Settings() {};
   
   public static final String NAME = "pattypan";
-  public static final String VERSION = "0.0.0";
+  public static final String VERSION = "0.1.0";
   public static final String USERAGENT = NAME + "/" + VERSION + " (https://github.com/yarl/pattypan)";
 
   public static int WINDOW_WIDTH = 600;
