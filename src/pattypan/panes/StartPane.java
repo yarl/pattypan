4e5f0f643908f485efe15ffb2a32efb71058e8e6
Paweł Marynowski <pawel.marynowski@solution4future.com>
2016-07-22 22:01:21 +0200

    bigger buttons on start screen
    plus move bug info text to properties

--- src/pattypan/panes/StartPane.java
+++ src/pattypan/panes/StartPane.java
@@ -125,17 +125,17 @@ public class StartPane extends GridPane {
     this.addRow(1, new WikiLabel("v. " + Settings.VERSION));
 
     this.addRow(20, new HBox(20,
-            new WikiButton("start-generate-button", "primary").setWidth(200).linkTo("ChooseDirectoryPane", stage),
-            new WikiButton("start-validate-button").setWidth(200).linkTo("LoadPane", stage)));
+            new WikiButton("start-generate-button", "primary").setWidth(300).linkTo("ChooseDirectoryPane", stage),
+            new WikiButton("start-validate-button").setWidth(300).linkTo("LoadPane", stage)));
 
-    this.addRow(21, new HBox(20,
-            new WikiLabel("start-generate-description").setWidth(200),
-            new WikiLabel("start-validate-description").setWidth(200)));
+    this.addRow(22, new HBox(20,
+            new WikiLabel("start-generate-description").setWidth(300),
+            new WikiLabel("start-validate-description").setWidth(300)));
 
     this.addRow(40, new WikiLabel("2016 // Pawel Marynowski").setClass("muted"));
 
-    Hyperlink link = new Hyperlink("Report it!");
-    TextFlow flow = new TextFlow(new Text("Found bug?"), link);
+    Hyperlink link = new Hyperlink(Util.text("start-bug-report"));
+    TextFlow flow = new TextFlow(new Text(Util.text("start-bug-found")), link);
     flow.setTextAlignment(TextAlignment.CENTER);
     link.setOnAction(event -> {
       try {
