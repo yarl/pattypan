yarl/pattypan

commit 6d8f7b84945742b99bb78766d247e5ceafb26768

fix(LoadPane): invert logic for clarity

src/pattypan/panes/LoadPane.java
@@ -156,7 +156,7 @@ public class LoadPane extends WikiPane {
         if (description.get("name").isEmpty()) {
           throw new Exception("empty name");
         }
-        if (!description.get("path").startsWith("https://") && !description.get("path").startsWith("http://")) {
+        if (description.get("path").startsWith("https://") || description.get("path").startsWith("http://")) {
           if (!checkIfFileExist(description.get("path"))) {
             throw new Exception("file not found");
           }

commit 4bfdfd81a952de4575fb41b8e11ba469264befc7

Fix things caused by 6d8f7b8

src/pattypan/panes/LoadPane.java
@@ -125,12 +125,9 @@ public class LoadPane extends WikiPane {
     String fixedPath = path.trim()
             .replace("/", File.separator)
             .replace("\\", File.separator);
-    try {
-      URL url = new URL(fixedPath);
-      return true;
-    } catch (MalformedURLException ex) {
-      return false;
-    }
+
+      File file = new File(fixedPath);
+      return file.isFile();
   }
 
   /**
@@ -163,10 +160,6 @@ public class LoadPane extends WikiPane {
           throw new Exception("empty name");
         }
         if (description.get("path").startsWith("https://") || description.get("path").startsWith("http://")) {
-          if (!checkIfFileExist(description.get("path"))) {
-            throw new Exception("file not found");
-          }
-        } else {
           if (!Util.validUrl(description.get("path"))) {
             throw new Exception("invalid URL");
           }
@@ -174,6 +167,10 @@ public class LoadPane extends WikiPane {
           // when uploaded from URL the extension is not automatically added
           if (!Util.stringHasValidFileExtension(description.get("name"))) {
             throw new Exception("filename does not include a valid file extension");
+          }
+        } else {
+          if (!checkIfFileExist(description.get("path"))) {
+            throw new Exception("file not found");
           }
         }
 